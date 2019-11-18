800b2cf8857f880696d7aa89561ff9b5fdd14513
hamyduong <57398988+hamyduong@users.noreply.github.com>
2019-11-18 20:22:27 +0100

    Add files via upload

--- match.cpp
+++ match.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <set>
 using namespace std; //on peut?
 
 
 // Program prints the name of the exact sequence in the database:equality test between the query and the whole database
 // NB:the query can have a fake name, makefile to do too
 // Parameter: argv[1]=query (FASTA), argv[2]=database (BLAST)
-// compile : g++ match.cpp -o match -Wall
-// execute : ./match fichier1 fichier2
+// pour compiler : g++ match.cpp -o match -Wall
+// pour executer : ./match fichier1 fichier2
 
 int main(int argc, char *argv[])
 {
 	
 	
 	// verify that we have 3 arguments, the name of the program and the 2 files
-	if (argc < 4) {
+	if (argc < 3) {
 		printf("Error: need 2 arguments \n");
 		return 1;
 	}
@@ -27,31 +28,60 @@ int main(int argc, char *argv[])
 	// FASTA: la requete est donnee sous forme fasta cad qu'on a >l'identifiant, le commentaire puis toute la séquence
 	// doit donc ignorer l'identifiant car peut être faux
 	// 80 charact par ligne? à verifier
-	ifstream query_file (argv[1], ios::in | ios::binary);
-	if( query_file.is_open() )
+	// on prend donc ligne par ligne : stocke dans un string?
+	ifstream query_file (argv[1]);
+	if( !query_file.is_open() )
 	{
-		//. . .
-		query_file.close();
-	}
-	else{
-		cout << "Impossible d'ouvrir le fichier" << endl;
+		cout << "Impossible to open the file" << endl;
 		return 1;//retourne 1 en cas d'erreur
 	}
+	
+	// on contient tout dans un string
+	/*string line, name, content;
+    while( getline(query_file,line).good() ){ 
+        if(line.empty() || line[0] == '>' ){ // lecture du fichier 
+            if( !name.empty() ){ // lire le fichier 
+                cout << name << " : " << content << endl;
+                name.clear();
+            }
+            if( !line.empty() ){
+                name = line.substr(1);
+            }
+            content.clear();
+        } else if( !name.empty() ){
+            if( line.find(' ') != std::string::npos ){ // en cas d'erreur d'espacement de sequence 
+                name.clear();
+                content.clear();
+            } else {
+                content += line;
+            }
+        }
+    }
+    if( !name.empty() ){ // Print out les fichies 
+		cout << name << " : " << content << endl;
+    }*/
+	query_file.close();
 
+	string argv2 = argv[2];
+	argv2+=".psq";
 
-
-	//BLAST: index=info on database, sequence=info on residues, header=info on header of sequence
-	ifstream database_file (argv[2], ios::in | ios::binary);
-	if( database_file.is_open() )
+	// BLAST: index=info on database, sequence=info on residues, header=info on header of sequence
+	ifstream database_file (argv2, ios::in | ios::binary);
+	if( !database_file.is_open() )
 	{
-		//. . .
-		database_file.close();
-	}
-	else{
-		cout << "Impossible d'ouvrir le fichier" << endl;
+		cout << "Impossible to open the file" << endl;
 		return 1;
 	}
 	
-	
+    char sequence[80];
+    char sequenc[80];
+    char sequen[80];
+    database_file.read(sequence,80);
+	database_file.read(sequenc,80);
+	database_file.read(sequen,80);
+    cout << sequen << endl;
+    cout << sequence << endl;
+    cout << sequenc << endl;
+	database_file.close();
 	return 0;
 }
