9b5c7ceb1827625d406248e876dbe8050d394098
Elisegutermann <57399151+Elisegutermann@users.noreply.github.com>
2019-11-23 17:07:58 +0100

    Add files via upload
    code du match avec le comptage de bits
    j'ai mis des commentaires pour expliquer ce que j'ai fait

--- match.cpp
+++ match.cpp
@@ -11,8 +11,7 @@ using namespace std;
 /* Program prints the name of the exact sequence in the database:equality test between the query and the whole database
  * NB:the query can have a fake name, makefile to do too
  * Parameter: argv[1]=query (FASTA), argv[2]=database (BLAST)
- * pour compiler : g++ match.cpp -o match 
- * pour executer : ./match fichier1 fichier2*/
+*/
 
 int main(int argc, char *argv[])
 {
@@ -24,56 +23,39 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	
-
-	
-	/*TO DO:
-	 * on ignore la première ligne
-	 * on lit ligne par ligne, puis divise en caractère
-	 * grâce a la classe 'Letter', qui a pour attribut 'Name', on utilise une fonction 'BinaryConversion(Letter)'
-	 * renvoie un bitset<8>, cad la forme binaire de la lettre
-	 * on stocke chaque binaire dans query (vecteur)
-	*/
-	
 	
 	// open the fasta file with the query
 	ifstream query_file (argv[1]);
-	if( !query_file.is_open() )
+	if(!query_file.is_open())
 	{
 		cout << "Impossible to open the file" << endl;
 		return 1;//retourne 1 en cas d'erreur
 	}
 	string str;
-	
 	// ignore first line
     std::getline(query_file, str);
+    str.clear();
     
-    // c is the character in the file, s is the name of the Letter created
+    
+    // c is the character in the file, s is the name of the Letter created, query is a vector containing the binary form of each letter
 	char c;
 	string s = "";
-	std::vector<bitset<8>> tableau; //crée un tableau de type bitset<8> vide
+	std::vector< bitset<8> > query; //crée un tableau de type bitset<8> vide
 		while(query_file >> c)
 		{	
 			s=c;
 			Letter* lettre = new Letter(s);
 			std::bitset<8> bit;
 			bit = lettre->binary_conversion();
-			cout << c << endl;
-			cout << bit << endl;
-			tableau.push_back(bit); //rajoute tous les chiffres en binaire dans le tableau
-			//cout << tableau << endl;
+			query.push_back(bit); //rajoute tous les chiffres en binaire dans le tableau
 		}
-		//permet de vérifier en print les 4 derniers elements du tableau
-		cout << tableau.size() << endl;
-		std::bitset<8> sequence;
-		for (int i=1206; i<tableau.size(); i++){
-			sequence = tableau[i];
-			cout << sequence << endl;
-		}
+
 	query_file.close();
 
 
 
 	// open the sequence BLAST file
+	
 	string argv2 = argv[2];
 	argv2+=".psq";
 	ifstream database_file (argv2, ios::in | ios::binary);
@@ -83,49 +65,102 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	/* on a stocké la query, maintenant on lit ligne par ligne le fichier .psq pour comparer avec la query
-	 * une fois qu'on trouve la bonne sequence, on ouvre le fichier index et on en retire l'offset
-	 * (comprendre comment on manipule les offsets/comment le fichier index est fait)
-	 * puis, avec offset header, on trouve le nom de la proteine dans le fichier header
-	 * (comprendre comment est constitué le fichier header)
-	 * */
-	 
-   /*std::bitset<8> b;
-    while(!database_file.eof())
-		{
-			b = database_file.get();
-			if(!database_file.eof())
-			//cout << b; imprimer toute la database
-			//il faut couper la sequence quand on a un bit nul cad 00000000 (8 bits)
-			//comparer la sequence coupee avec la query transformee en binaire*/
-
-        
-        
-	/*CODE POUR BOUCLE DE COMPARAISON (pas encore testé):
-	std::bitset<8> c;
-	while(!database_file.eof() && database_file.get()!=query)
-		{
-			c = database_file.get();
-			if(!database_file.eof()){
-				// manip avec offset etc.
+	
+	// b is the bit of the current letter read in the database, tmp is the current sequence read in the database
+	
+   std::bitset<8> b = 0b10000000;
+   std::vector< bitset<8> > tmp;
+   //on initialise un compteur de bits
+   int count = 0;
+   // parse the whole database
+    while( !database_file.eof() ){
+		b = database_file.get();
+		// if the bit read isn't null, we add it to the sequence
+		if (b != 0b00000000){ 
+			tmp.push_back(b);
+		}
+		
+		// b is null, the sequence ended and we can compare the two vectors 
+		
+		else{
+			//on incrémente le compteur de la taille du tmp considéré + les 8 zéros
+			count += tmp.size()+8;
+			// check if the two sequences are the same size, else we don't even look
+			if (query.size() == tmp.size()){
+				// check if every letter is the same
+				int breaking_out = 0;
+				for (int i = 0 ; (i < tmp.size())&&(query[i] == tmp[i]) ; i++){
+					// if we are at the end of the vector, every letter is the same and we break the while
+					if (i == (tmp.size()-1)){
+						cout << "Found the sequence!" << endl;
+						cout << "Le nombre de bits à cet endroit est de:" << count-10 << endl; // on imprime le count-10 pour être avant la fin de la séquence
+						breaking_out=1;
+						break;
+					}
+				}
+				if(breaking_out==1){
+					break;
+				}
+				// if we didn't found, reset the vector continue the loop
+				tmp.clear();
+				
+			}
+			
+			else{
+				// if the sizes aren't the same, continue the while loop
+				tmp.clear();
 			}
 		}
-	*/
-		
-		
+	}
+
+	database_file.close();
+
+	/* TO DO:
+	 * on va faire un compteur pour savoir à quel nombre de bits on est
+	 * avec ce nombre de bits, on se repère dans le tableau offset sequence
+	 * offset sequence donne, à chaque indice, à cb de bits on est
+	 * l'indice dans les deux tableaux offset est le mm
+	 * simple title est le nom de la sequence ds le fichier header
+	 * */
+	 
+	 
+	 
 	/*	CODE POUR OUVRIR LE FICHIER PHR
-	char c;
-	while(!database_file.eof())
+	string argv3 = argv[3];
+	argv3+=".phr";
+	ifstream header_file (argv3, ios::in | ios::binary);
+	if( !header_file.is_open() )
 	{
-		c = database_file.get();
-		if(!database_file.eof()){
-		cout << c;
+		cout << "Impossible to open the file" << endl;
+		return 1;
+	}
+	char t;
+	
+	while(!header_file.eof())
+	{
+		t = header_file.get();
+		if(!header_file.eof()){
+		cout << t;
 		}
 	}*/
+	/*
+	string argv4 = argv[4];
+	argv4+=".pin";
+	ifstream index_file (argv4, ios::in | ios::binary);
+	if( !index_file.is_open() )
+	{
+		cout << "Impossible to open the file" << endl;
+		return 1;
+	}
+	std::bitset<32> t;
 	
-	
-	
-	database_file.close();
+	while(!index_file.eof())
+	{
+		t = index_file.get();
+		if(!index_file.eof()){
+		cout << t << endl;
+		}
+	}
 	return 0;
-	
+	*/
 }
