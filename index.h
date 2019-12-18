8cfcef11b799fafdf5774304e2d5c705f0a45fbf
hamyduong <57398988+hamyduong@users.noreply.github.com>
2019-12-18 12:07:41 +0100

    Update index.h

--- index.h
+++ index.h
@@ -1,3 +1,4 @@
+
 #include <string>
 #include <iostream>
 #include <vector>
@@ -6,24 +7,19 @@ using namespace std;
 
 class Index
 {
-private:
+
+public:
 	int32_t version;
 	int32_t database_type;
 	int32_t title_length;
-	vector<char> title;
+	char* title;
 	int32_t timestp_length;
-	vector<char> timestp;
+	char* timestp;
 	int32_t nbseq;
 	int64_t residuecount;
 	int32_t maxseq;
-	vector<int32_t> header_offset;
-	vector<int32_t> sequence_offset;
-	std::string argv2;
-	int32_t header_offset[nbseq+1];
-	int32_t sequence_offset[nbseq+1];
-	char title[title_length];
-	char timestp[timestp_length];
-public:
-	Index(std::string argv2,int32_t version,int32_t database_type,int64_t residuecount,int32_t nbseq,int32_t maxseq); //constructor	
+	int32_t* header_offset;
+	int32_t* sequence_offset;
+	Index(std::string argv2); //constructor	
 	
 };
