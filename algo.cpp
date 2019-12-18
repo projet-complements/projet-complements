cb113b793902d82e204fb5ab3e8c454ae93373cf
hamyduong <57398988+hamyduong@users.noreply.github.com>
2019-12-18 12:54:23 +0100

    Update algo.cpp

--- algo.cpp
+++ algo.cpp
@@ -90,3 +90,88 @@ int* Algo::sw(){
 	}
 	return score;
 }
+
+/* Here is the other algorithm, optimized, that we have done. However, we do not have the right results..
+int* Algo::sw(){	
+	//initialize the values that we need
+	int* score;
+	score=new int[size]();
+	int Hdiag=0;
+	int E,currentH,m,res1,res2,res3;
+	int8_t b;
+	
+	// defines the penalties
+	int Q=open+ext;
+	int R=ext;
+
+	// parse the whole database
+	for (int ind =0; ind<(size) ;ind++){
+		// m is the size of the protein we are analyzing in the database
+		m=offset[ind+1]-offset[ind]-1;
+		
+		// we save one column of H and F, which are both initialize to 0
+		int* H = nullptr;
+		int* F = nullptr;
+		H=new int[n+1]();
+		F=new int[n+1]();
+		
+		// search for the highest score
+		for(int i = 0; i < m ; i++){
+			// for every new column, we set E, H diagonal and H vertical to 0
+			E=0;
+			Hdiag=0;
+			for(int j = 1; j < n+1 ; j++){
+				b = db[offset[ind]+i];
+				// we create a coord type, which will give us the score between two proteins
+				Coord* coord = new Coord(query[j-1],b,arg_blosum);
+				res2=Hdiag+coord->score();
+				
+				
+				// updates the value of E by choosing the maximum between E-R and H[j-1]-Q (H[j-1] being the value of the previous iteration)
+				if((E-R)>(H[j-1]-Q)){
+					E=E-R;
+				} 
+				else{
+				 	E=H[j-1]-Q; 
+				}
+				res1=E;
+				
+				if ((F[j]-R)>(Hdiag-Q)){
+					F[j]=F[j]-R;
+				}
+				else{
+					F[j]=Hdiag-Q;
+				}
+				res3=F[j];
+				
+				// search for the max between res1,2 and 3
+				currentH=0;
+				if (res1>currentH){
+					currentH=res1;
+				}
+				if (res3>currentH){
+					currentH=res3;
+				}
+				if (res2>currentH){
+					currentH=res2;
+				}
+
+				if (currentH>score[ind]){
+					score[ind]=currentH;
+				}
+				// we update everything for the next iteration
+				H[j]=currentH;
+				// the future diagonal H is the current H[j] we are looking
+				Hdiag=H[j];
+				
+				delete(coord);
+			}
+		}
+		//cout << "Algo est a calculé " << ind <<"avec comme score " << score[ind]<< endl;
+		delete(H);
+		delete(F);
+
+	}
+	return score;
+}
+*/
